c77e2687e30a775c85db26cfbc6892c7564e9b68
Minsun Lee <dalzony@gmail.com>
2018-08-28 16:27:38 +0900

    [05] ArithmaticOperator

---
+++ src/EX_ArithmeticOperator.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>):Unit
+{
+    val num:Int = 15 - 4*3
+    val num2:Int = 65 % 7
+    val num3:Double = 7.5/5 + 22.25
+    val num4:Double =num/num2 + 0.7 //num.toDouble()을 사용하면 소수점 보존된 결과를 얻을 수 있다.
+
+    println(num)
+    println(num2)
+    println(num3)
+    println(num4)
+}
