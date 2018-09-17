bace80d8ac44746fd6db44ad28af46907505a009
Minsun Lee <dalzony@gmail.com>
2018-09-17 15:30:56 +0900

    ~[67]까지 중에 설명 첨언

--- part2/src/ex_inheritance/Main.kt
+++ part2/src/ex_inheritance/Main.kt
@@ -2,9 +2,16 @@ package ex_inheritance
 
 open class Person(val name:String, val age:Int)
 
+/*
 class Student(name: String, age:Int, val id:Int): Person(name,age)
+
+*/
+open class Student(name: String, age:Int, val id:Int): Person(name,age)
+open class Teacher(name: String, age:Int, id:Int, subject: String):Student(name,age,id)
 
 fun main(args:Array<String>) {
     val person = Person("lee", 35)
     val student = Student("kim", 23,20171217)
+    val teacher = Teacher("mk", 40, 302304, subject = "eng")
 }
+//64 구리지만; Student도 상속이 가능한 클래스로 바꿔
--- part2/src/ex_overriding/Main.kt
+++ part2/src/ex_overriding/Main.kt
@@ -2,22 +2,28 @@ package ex_overriding
 
 open class AAA
 {
-    open fun func() = println("AAA")
+    open fun func() = println("AAA") //open을 써야 오버라이딩이 가능
+    open fun testadd() = println("AAAadd") //테스트용
 }
 
 class BBB:AAA()
 {
-    override fun func()
+    override fun func() //overrice를 써야함
     {
         super.func()
         println("BBB")
     }
+
+    //override fun testadd() = println("BBBADD") 테스트용
 }
 
 fun main(args:Array<String>)
 {
     AAA().func()
+    println("------------------")
     BBB().func()
+    BBB().testadd()
 }
 
+// 한번 override 되면 하위에서 계속 오버라이딩 가능하므로, 막으려면 final을 사용
 //66
